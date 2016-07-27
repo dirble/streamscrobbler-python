c05dd807f32bb9fba06aa70e5eae5ece34ce668d
GijsTimmers <timmers.gijs@gmail.com>
2016-07-27 02:19:22 +0200

    Created a fully working setup.py file
    Please use this file to upload to PyPI after registering there:
    
    $ sudo python setup.py sdist upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+import os
+
+
+setup(
+name = "streamscrobbler",
+packages = ["streamscrobbler"],
+version = "0.0.1",
+description = "A python class used on Dirble.com to get titles and bitrates on shoutcast and icecast streams",
+author = "Håkan Nylén",
+author_email = "confacted@gmail.com",
+url = "https://github.com/Dirble/streamscrobbler-python",
+keywords = ["shoutcast", "icecast", "stream", "ICY"],
+install_requires = ["httplib2"],
+classifiers = [],
+include_package_data = True
+)
